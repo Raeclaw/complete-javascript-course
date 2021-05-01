6df47df691e9505ff58aa3663085e105198f9eff
Raeclaw <80207968+Raeclaw@users.noreply.github.com>
2021-05-02 01:05:50 +0200

    asda

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = `amazing`;
 console.log(40 + 6 - 85 - 2);
 
@@ -5,3 +6,62 @@ console.log(`Jonas`);
 
 let firstName = `Bob`;
 console.log(firstName);
+
+// // My first challenge - start
+
+// let country = `Poland`;
+// let continent = `Europe`;
+// let population = `38`;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// // My first challenge - end
+
+true;
+console.log(true)
+let javaScript = 2;
+
+console.log(typeof true);
+console.log(typeof javaScript);
+console.log(typeof 33);
+console.log(typeof `true`);
+
+javaScript = true;
+console.log(javaScript);
+
+let year;
+console.log(year)
+console.log(typeof year)
+
+year = 1991;
+
+console.log(year)
+console.log(typeof year)
+
+console.log(typeof null);
+
+
+// // My second challenge - start
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland, typeof population, typeof country, typeof language);
+// // My second challenge - end
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1987;
+
+var job = `programmer`;
+job = `teacher`;
+*/
+
+// // My third challenge - start
+// let language = `Polish`;
+// const faithful = true;
+// faithful = false;
+// // My third challenge - end
+
